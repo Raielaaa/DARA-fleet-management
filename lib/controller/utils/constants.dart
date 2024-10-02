91ca2e34027451d8940c5fb55b3125d74e1ad4ef
Ralph Daniel Honra <rbhonra@ccc.edu.ph>
2024-10-03 03:40:08 +0800

    v7.6 facebook redirect

--- lib/controller/utils/constants.dart
+++ lib/controller/utils/constants.dart
@@ -2,5 +2,4 @@ class Constants {
   static const String OPEN_WEATHER_API_KEY = "f30c3d57030287b26643ab8f123d8db3";
   static const String GOOGLE_MAPS_API_KEY = "AIzaSyBdOBiE5grXG_Xyt5qL4s19qBt-JK_2rTo";
   static const String PAYMONGO_PUBLIC_KEY = "pk_live_rpVTFVfax4Vdrf8mNB6LiSbi";
-
 }
